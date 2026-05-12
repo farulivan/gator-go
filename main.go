e10dfa1800f2452e9dd55368a8816d9aada928ea
farulivan <farul@nabitu.id>
2026-05-12 16:02:04 +0700

    feat: add command registry and login handler 🎯

--- main.go
+++ main.go
@@ -1,27 +1,37 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"os"
 
 	"github.com/farulivan/gator-go/internal/config"
 )
+
+type state struct {
+	cfg *config.Config
+}
 
 func main() {
 	cfg, err := config.Read()
 	if err != nil {
 		log.Fatalf("error reading config: %v", err)
 	}
-	fmt.Printf("Read config: %+v\n", cfg)
 
-	err = cfg.SetUser("farul")
-	if err != nil {
-		log.Fatalf("couldn't set current user: %v", err)
+	s := &state{cfg: &cfg}
+
+	cmds := commands{registeredCommands: make(map[string]func(*state, command) error)}
+	cmds.register("login", handlerLogin)
+
+	if len(os.Args) < 2 {
+		log.Fatal("usage: gator <command> [args...]")
 	}
 
-	cfg, err = config.Read()
-	if err != nil {
-		log.Fatalf("error reading config: %v", err)
+	cmd := command{
+		name: os.Args[1],
+		args: os.Args[2:],
 	}
-	fmt.Printf("Read config again: %+v\n", cfg)
+
+	if err := cmds.run(s, cmd); err != nil {
+		log.Fatal(err)
+	}
 }
