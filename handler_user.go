8feb7ceb6a5558afaad36adb1872ead4bb35c00b
farulivan <farul@nabitu.id>
2026-05-13 19:31:33 +0700

    feat: add users command to list all registered users 👥

--- handler_user.go
+++ handler_user.go
@@ -72,3 +72,20 @@ func handlerReset(s *state, cmd command) error {
 	fmt.Println("All users have been deleted")
 	return nil
 }
+
+func handlerUsers(s *state, cmd command) error {
+	users, err := s.db.GetUsers(context.Background())
+	if err != nil {
+		return fmt.Errorf("failed to get users: %w", err)
+	}
+
+	fmt.Println("Users:")
+	for _, user := range users {
+		if user.Name == s.cfg.CurrentUserName {
+			fmt.Printf("  * %s (current)\n", user.Name)
+		} else {
+			fmt.Printf("  * %s\n", user.Name)
+		}
+	}
+	return nil
+}
