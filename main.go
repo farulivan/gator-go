042430d367ed23da0eaf988a17c75d3a92e7cc3e
farulivan <farul@nabitu.id>
2026-05-13 19:58:49 +0700

    feat: add RSS feed fetching with agg command 📡

--- main.go
+++ main.go
@@ -36,6 +36,7 @@ func main() {
 	cmds.register("register", handlerRegister)
 	cmds.register("reset", handlerReset)
 	cmds.register("users", handlerUsers)
+	cmds.register("agg", handlerAgg)
 
 	if len(os.Args) < 2 {
 		log.Fatal("usage: gator <command> [args...]")
